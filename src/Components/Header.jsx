414e548be38aceca37e01e4dc5177b51ed2af0a4
Jason Chan <jason@Jasons-Air.home>
2023-03-15 15:43:34 +0000

    added a logo and css styling

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+
 export const Header = () => {
   return (
     <header>
       <h1>
         <Link to="/" className="header">
-          Games Review
+          <img src={require("./logo.png")} alt="logo" id="logo" />
         </Link>
       </h1>
     </header>
